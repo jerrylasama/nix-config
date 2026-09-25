6dd7200062c58762741729ad7e1b6fdde3bf6651
jerry <jerrylasama@gmail.com>
2026-09-25 16:19:35 +0000

    build(flake): drop unused deploy-rs input

--- flake.nix
+++ flake.nix
@@ -22,11 +22,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
